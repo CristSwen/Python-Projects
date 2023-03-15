fb2bd386b1a83f72dc3be18136efc8b76c0d1d19
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-03-15 15:44:50 -0600

    Created Inheritance.py
    Created a file that was originally made in python IDLE that demonstrates how child classes can inherit the parent class

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Store:
+    name = 'No name Provided'
+    location = ' '
+
+class Company(Store):
+    storeType = 'Grocery'
+    affordability = 'expensive'
+
+class Product(store):
+    foodType = 'healthy'
+    productAmount = 5000
